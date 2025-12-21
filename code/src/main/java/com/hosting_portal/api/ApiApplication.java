a8868e34c01c20dcf81e126736f38b2499742552
Titan327 <tristan.tourbier@supinfo.com>
2025-12-22 00:10:12 +0100

    init spring

---
+++ code/src/main/java/com/hosting_portal/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.hosting_portal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
